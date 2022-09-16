78e4834f3d73bbaee9fa57f12ebbb1c5f2c0a588
Haphezz <haphezzabubakar11@gmail.com>
2022-09-16 07:42:46 -0700

    uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,12 +8,12 @@
  * @c: checks input of function
  *
  * Return: returns 1 if 'c' is uppercase
- * 	otherwise always 0 (Success)
+ * 	otherwise always 0 (success)
  */
 
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
- return (1);
+return (1);
 return (0);
 }
